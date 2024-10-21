e4c4ceade9961daa2770f32862e51474350cff02
Stan004 <flywwww004@gmail.com>
2024-10-21 22:28:27 +0800

    fix: production evironment issue, try fixing-8 adjust prisma.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -5,8 +5,8 @@ export async function fetchMonthlyBalance( queryDate: Date  ){
     
     console.log('Fetching monthly balance with date', queryDate);
 
-    const testData = await prisma.balance.findFirst();
-    console.log('Fetching monthly first testing data', testData);
+    //const testData = await prisma.balance.findFirst();
+    //console.log('Fetching monthly first testing data', testData);
 
     try {
         const data = await prisma.balance.findMany({
