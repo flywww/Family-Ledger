5ea823f52bf143eed57f2c2227cd1001fd9453e8
Stan004 <flywwww004@gmail.com>
2024-10-21 22:51:52 +0800

    fix: modify actions- query date instead of range

--- lib/actions.ts
+++ lib/actions.ts
@@ -5,23 +5,9 @@ export async function fetchMonthlyBalance( queryDate: Date  ){
     
     console.log('Fetching monthly balance with date', queryDate);
 
-    //const testData = await prisma.balance.findFirst();
-    //console.log('Fetching monthly first testing data', testData);
-
-    const startOfDay = new Date(queryDate);
-    startOfDay.setUTCHours(0, 0, 0, 0);
-
-    const endOfDay = new Date(queryDate);
-    endOfDay.setUTCHours(23, 59, 59, 999);
-
     try {
         const data = await prisma.balance.findMany({
-            where:{ 
-                date: {
-                    gte: startOfDay,
-                    lte: endOfDay,
-                  },
-             }
+            where:{ date: queryDate }
         })
         return data;
     } catch (error) {
